2caa760147924c4f77448b3ecc61f749cf222b9b
NicMortelliti <npmortelliti@gmail.com>
2022-07-21 20:05:17 -0700

    commit tree ebc67bfb45e977201e90e43de5f621131ab84a33

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = []
+
+# Line method
+def line(arr)
+  if arr.length.zero?
+    puts 'The line is currently empty.'    
+  else
+    output = 'The line is currently:'
+    
+    arr.each do |i|
+      output << " #{arr.index(i)+1}. #{i}"
+    end
+    puts output
+  end
+end
+
+# Take a number method
+def take_a_number(arr, name)
+  arr << name
+  puts "Welcome, #{name}. You are number #{arr.length} in line."
+end
+
+# Now serving method
+def now_serving(arr)
+  if arr.length.zero?
+    puts 'There is nobody waiting to be served!'
+  else
+    puts "Currently serving #{arr[0]}."
+    arr.shift
+  end
+end
